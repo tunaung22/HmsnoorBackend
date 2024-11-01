018e8be56bf6cd14e53f032ea3a5cc78698b5cc6
Tun Tun Aung <tunaung2048@gmail.com>
2024-11-01 17:46:10 +0700

    Alternative approach to configure table entity using

---
+++ HmsnoorBackend/Data/Configurations/ItemHeadersModelCreatingExtensions.cs
@@ -0,0 +1,44 @@
+using System;
+using HmsnoorBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HmsnoorBackend.Data.Configurations;
+
+
+// Usage at db context onModelCreating:
+// protected override void OnModelCreating(ModelBuilder builder)
+// {
+//     builder.ConfigureItemHeaders();
+// }
+
+public static class ItemHeadersModelCreatingExtensions
+{
+    public static void ConfigureItemHeaders(this ModelBuilder builder)
+    {
+        builder.Entity<ItemHeader>(b =>
+        {
+            b.ToTable("ItemHeader");
+            b.HasKey(e => new { e.ItemNo, e.ItemType });
+
+            b.Property(e => e.ItemNo)
+                .HasColumnName("ItemNo")
+                .HasMaxLength(10);
+
+            b.Property(e => e.ItemName)
+                .HasColumnName("ItemName")
+                .HasMaxLength(200);
+
+            b.Property(e => e.ItemType)
+                .HasColumnName("ItemType")
+                .HasMaxLength(100);
+
+            b.Property(e => e.ItemCategory)
+                .HasColumnName("ItemCategory")
+                .HasMaxLength(500);
+
+            b.Property(e => e.MItemName)
+                .HasColumnName("MItemName")
+                .HasMaxLength(500);
+        });
+    }
+}
