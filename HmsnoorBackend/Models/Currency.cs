995d043f2d11addd22def6a41861a0f9d6d0cbf0
Tun Tun Aung <tunaung2048@gmail.com>
2024-11-09 14:46:59 +0700

    Added Currency

---
+++ HmsnoorBackend/Models/Currency.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace HmsnoorBackend.Models;
+
+public partial class Currency
+{
+    public int CurrencyId { get; set; }
+    public string CurrencyName { get; set; } = null!;
+
+}
